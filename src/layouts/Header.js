576b6a728c188d223becbba0287e846c76743407
ritin0204 <ritintiwari417@gmail.com>
2024-01-08 23:30:44 +0530

    made responsive on mobile device

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../assests/images/logo-white.png';
 import {NavLink} from 'react-router-dom';
 
 const Header = () => {
+  const [showNav, setShowNav] = useState(false);
+  console.log(showNav);
+  const showNavbar = () => {
+    setShowNav(!showNav);
+  };
   return (
     <header>
       <div className='logo-div'>
         <img src={logo} alt='My Journal Logo' className='logo-img'></img>
       </div>
-      <nav id='nav-bar'>
-        <NavLink to="/" exact>
+      <nav id='nav-bar' className={showNav ? 'show': ''}>
+        <NavLink to="/" exact onClick={showNavbar}>
           Dashboard
         </NavLink>
-        <NavLink to="/journals" exact>
+        <NavLink to="/journals" exact onClick={showNavbar}>
           Journal
         </NavLink>
-        <NavLink to="/activities" exact>
+        <NavLink to="/activities" exact onClick={showNavbar}>
           Activities
         </NavLink>
-        <NavLink to="/account" exact>
+        <NavLink to="/account" exact onClick={showNavbar}>
           Account
         </NavLink>
       </nav>
+      <div className='burger-div' onClick={showNavbar}>
+        <div className='burger-inner-div'></div>
+        <div className='burger-inner-div'></div>
+        <div className='burger-inner-div'></div>
+      </div>
     </header>
   );
 };
